f794f01013ba733122586d285c9d6ee9bb337e0e
JAJA <abdelkarimjaja1@gmail.com>
2024-01-30 17:33:22 +0100

    started customer-manager api

---
+++ app/Http/Resources/V1/InvoiceResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InvoiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+	/**
+	 * invoices tables columns name:
+	 *  customer_id
+	 *  amount  
+	 *  status 
+	 *  billed_date
+	 *  paid_date
+	 *
+	 */
+    public function toArray(Request $request): array
+    {
+	    return [
+		    'id'=> $this->id,
+		    'customerId'=> $this->customer_id,
+		    'status'=> $this->status,
+		    'billedDate'=> $this->billed_date,
+		    'paidDate'=> $this->paid_date,
+	    ];
+    }
+
+}
