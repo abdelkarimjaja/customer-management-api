f5f1316e2f4a953c9eac1590e400d726efc9401d
JAJA <abdelkarimjaja1@gmail.com>
2024-01-31 10:29:17 +0100

    added filters to api

---
+++ app/Filters/V1/InvoiceFilter.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+/**
+ * 
+	    $table->integer('customer_id');
+	    $table->integer('amount');
+	    $table->string('status');
+	    $table->dateTime('billed_date');
+	    $table->dateTime('paid_date')->nullable();
+ *
+ */
+
+class InvoiceFilter extends ApiFilter{
+
+	protected $safeParms = [
+		'customerId' => ['eq'],
+		'amount' => ['eq', 'gt', 'lt'],
+		'status' => ['eq','ne'],
+		'billedDate' => ['eq', 'gt', 'lt'],
+		'paidDate' => ['eq', 'gt', 'lt'],
+	];
+
+	protected $columnMap = [
+		'customerId' => 'customer_id',
+		'billedDate' => 'billed_date',
+		'paidDate' => 'paid_date'
+	];
+
+	protected $operatorMap = [
+		'eq' => '=',
+		'lt' => '<',
+		'gt' => '>',
+		'ne' => '!='
+	];
+
+}
+
